06c93db329806af0a7d95231b1329a73a1caadd3
awshum <alvinwshern@gmail.com>
2018-11-14 15:41:19 +0800

    Working script

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # scrapejs
 Scraping JS rendered websites with Python.
+
+## Installation
+You will need to install the Python libraries in requirements.txt.
+```
+pip install -r requirements.txt
+```
+You will also need PhantomJS webdriver for selenium.
+```
+brew install phantomjs
+```
+
+## Configuration
+- Enable Google Sheets API on your gmail account
+- Download the json credentials to your computer and update the file path in the script
+- On first run, you will be promopted to give permissions which will be stored in token.json
+- Configure the API scope and the Google Sheet on config.json
--- main.py
+++ main.py
@@ -1,35 +1,43 @@
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
-SPREADSHEET_ID = '1a_I2ok0TGM95sKDncuUpPLCb3cTz1agcq8RsMPUvXCU'
-RANGE_NAME = 'Sheet1!A:Z'
-
-def write_sheet(service, rows):
+def write_sheet(service, rows, spreadsheet_id, range_name):
     body = {
-        'values': rows
+        'values': [rows]
     }
-    result = service.spreadsheet().values().update(
-        spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME,
+    print(service)
+    result = service.spreadsheets().values().update(
+        spreadsheetId=spreadsheet_id, range=range_name,
         valueInputOption='USER_ENTERED', body=body).execute()
     print('{0} cells updated.'.format(result.get('updatedCells')));
 
-def scrape():
-    pass
+def scrape(url, xpath):
+    driver = webdriver.PhantomJS()
+    driver.get(url)
+    element = driver.find_element(By.XPATH, xpath)
+    return element.text
 
 def main():
-    store = file.Storage('../token.json')
+    with open('config.json', 'r') as f:
+        config = json.loads(f.read())
+        SCOPES = config[0]['SCOPES']
+        SPREADSHEET_ID = config[0]['SPREADSHEET_ID']
+    RANGE_NAME = 'Sheet1!A5'
+    store = file.Storage('./token.json')
     creds = store.get()
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('../credentials.json', SCOPES)
+        flow = client.flow_from_clientsecrets('./credentials.json', SCOPES)
         creds = tools.run_flow(flow, store)
     service = build('sheets', 'v4', http=creds.authorize(Http()))
-    result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,
-        range=RANGE_NAME).execute()
     # Call scrape function
+    value = [scrape('https://finance.yahoo.com/quote/%5ESTI', '//*[@id="quote-header-info"]/div[3]/div/div/span[1]')]
     # Write Outputs to sheet
-
+    write_sheet(service, value, SPREADSHEET_ID, RANGE_NAME)
 
 if __name__ == "__main__":
     main()
